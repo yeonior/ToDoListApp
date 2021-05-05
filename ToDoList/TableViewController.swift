e8705dcf3e9da4325af1899377ef8d1615685cdb
Rus <de.mr44@gmail.com>
2021-05-05 18:37:25 +0500

    Added placing tasks in the table

---
+++ ToDoList/TableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TableViewController.swift
+//  ToDoList
+//
+//  Created by ruslan on 05.05.2021.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var tasks: [String] = []
+
+    @IBAction func saveTask(_ sender: UIBarButtonItem) {
+        
+        let alertController = UIAlertController(title: "New Task", message: "Please add a new task", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+            let tf = alertController.textFields?.first
+            if let newTask = tf?.text {
+                self.tasks.insert(newTask, at: 0)
+                self.tableView.reloadData()
+            }
+        }
+        alertController.addTextField { _ in }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in }
+        
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = tasks[indexPath.row]
+
+        return cell
+    }
+}
